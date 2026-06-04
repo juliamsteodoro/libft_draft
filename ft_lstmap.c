091310e6a2a3f2d28dae88d4f4baca359fd1de65
Julia Moreira Dos Santos Teodoro <jumoreir@c3r3p5.42sp.org.br>
2026-06-04 13:32:42 -0300

    Norminette: OK! | libft-war-machine: OK! | libft-god: OK! | tripouille: OK

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -16,15 +16,19 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void(*del)(void *))
 {
 	t_list	*new_list;
 	t_list	*new_node;
+	void	*content;
 
 	if (!lst || !f || !del)
 		return (NULL);
 	new_list = NULL;
 	while (lst)
 	{
-		new_node = ft_lstnew(f(lst->content));
-		if (!new_node)
+		content = f(lst->content);
+		new_node = ft_lstnew(content);
+		if (!content || !new_node)
 		{
+			if (content)
+				del(content);
 			ft_lstclear(&new_list, del);
 			return (NULL);
 		}
